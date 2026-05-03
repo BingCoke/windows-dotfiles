c9acaad8dea1087dbaeebeb9faa521832369fd08
kryond <kryond@163.com>
2026-05-03 11:46:33 +0800

    set url formt from env

--- dot_config/nvim/lua/plugin.lua
+++ dot_config/nvim/lua/plugin.lua
@@ -1,13 +1,13 @@
-
+local url_format = os.getenv("NVIM_LAZY_URL_FORMAT") or 'https://github.com/%s.git'
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local lazyrepo = string.format(url_format, "folke/lazy.nvim")
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -21,6 +21,9 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     { import = "lazys" },
+  },
+  git = {
+    url_format = url_format,
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
