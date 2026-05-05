6123e44a8c5b5c5c56130d6390fef9900f8a46e7
kryond <kryond@163.com>
2026-05-05 14:52:11 +0800

    oversser keymap

--- dot_config/nvim/lua/lazys/overseer.lua
+++ dot_config/nvim/lua/lazys/overseer.lua
@@ -4,7 +4,15 @@ return {
 
     event = "VeryLazy",
     config = function()
-      require("overseer").setup()
+      require("overseer").setup({
+        task_list = {
+          keymaps = {
+
+            ["r"] = { "keymap.run_action", opts = { action = "restart" }, desc = "restart task" },
+            ["s"] = { "keymap.run_action", opts = { action = "stop" }, desc = "stop task" }
+          },
+        }
+      })
     end
   }
 }
