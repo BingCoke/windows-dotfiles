8a4713ff35612a003170d2db98501840d9b3f67b
kryond <kryond@163.com>
2026-06-28 14:05:15 +0800

    toggle term

--- dot_config/nvim/lua/lazys/toggleTerm.lua
+++ dot_config/nvim/lua/lazys/toggleTerm.lua
@@ -17,6 +17,60 @@ return {
 
       local Terminal = require("toggleterm.terminal").Terminal
 
+      local toggle_origins = {}
+
+      local function terminal_state()
+        local bufnr = vim.api.nvim_get_current_buf()
+
+        if not vim.api.nvim_buf_is_valid(bufnr) or vim.bo[bufnr].buftype ~= "terminal" then
+          return nil
+        end
+
+        return {
+          bufnr = bufnr,
+          mode = vim.fn.mode(1),
+          cursor = vim.api.nvim_win_get_cursor(0),
+        }
+      end
+
+      local function remember_origin(name, term)
+        local origin = terminal_state()
+
+        if term:is_open() then
+          if origin and vim.api.nvim_get_current_buf() ~= term.bufnr then
+            toggle_origins[name] = origin
+          end
+          return
+        end
+
+        toggle_origins[name] = origin
+      end
+
+      local function restore_origin(name)
+        local origin = toggle_origins[name]
+        toggle_origins[name] = nil
+
+        if not origin or origin.mode ~= "t" then
+          return
+        end
+
+        vim.schedule(function()
+          if
+            vim.api.nvim_get_current_buf() ~= origin.bufnr
+            or not vim.api.nvim_buf_is_valid(origin.bufnr)
+            or vim.bo[origin.bufnr].buftype ~= "terminal"
+          then
+            return
+          end
+
+          if origin.cursor then
+            pcall(vim.api.nvim_win_set_cursor, 0, origin.cursor)
+          end
+          vim.b[origin.bufnr].terminal_restore_insert = true
+          vim.cmd("startinsert")
+        end)
+      end
+
       local function global_cwd()
         return vim.fn.getcwd(-1, -1)
       end
@@ -34,9 +88,14 @@ return {
         on_open = function()
           vim.cmd("startinsert!")
         end,
+        on_close = function()
+          restore_origin("term")
+        end,
       })
 
       map({ "n", "i", "t" }, "<M-e>", function()
+        remember_origin("term", M.term)
+
         local dir = global_cwd()
         if M.term.job_id and M.term.bufnr and vim.api.nvim_buf_is_valid(M.term.bufnr) then
           M.term:change_dir(dir)
@@ -56,9 +115,14 @@ return {
         on_open = function(term)
           vim.cmd("startinsert!")
         end,
+        on_close = function()
+          restore_origin("git")
+        end,
       })
 
       map({ "n", "i", "t" }, "<M-g>", function()
+        remember_origin("git", M.git)
+
         local dir = global_cwd()
         if M.git.dir ~= dir and M.git.bufnr and vim.api.nvim_buf_is_valid(M.git.bufnr) then
           M.git:shutdown()
