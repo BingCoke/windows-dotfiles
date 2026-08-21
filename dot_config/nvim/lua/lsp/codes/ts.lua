ec0c0d6fed7a1c69e58cd5ed902c8e08e25872c7
kryond <kryond@hjw-tech.com>
2026-08-21 10:11:17 +0800

    1

--- dot_config/nvim/lua/lsp/codes/ts.lua
+++ dot_config/nvim/lua/lsp/codes/ts.lua
@@ -1,72 +1,80 @@
 local M = {}
 
 M.setup = function()
-	local vue_language_server_path = vim.fn.expand("$MASON/packages")
-		.. "/vue-language-server"
-		.. "/node_modules/@vue/language-server"
+  local vue_language_server_path = vim.fn.expand("$MASON/packages")
+      .. "/vue-language-server"
+      .. "/node_modules/@vue/language-server"
 
-	local tsserver_filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
-	local vue_plugin = {
-		name = "@vue/typescript-plugin",
-		location = vue_language_server_path,
-		languages = { "vue" },
-		configNamespace = "typescript",
-	}
+  local tsserver_filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
+  local vue_plugin = {
+    name = "@vue/typescript-plugin",
+    location = vue_language_server_path,
+    languages = { "vue" },
+    configNamespace = "typescript",
+  }
 
-	local ts_ls_config = {
-		init_options = {
-			plugins = {
-				vue_plugin,
-			},
-		},
-		filetypes = tsserver_filetypes,
-	}
+  local ts_ls_config = {
+    init_options = {
+      plugins = {
+        vue_plugin,
+      },
+    },
+    on_attach = function(client)
+      local existing_capabilities = client.server_capabilities
+      if vim.bo.filetype == 'vue' then
+        existing_capabilities.semanticTokensProvider.full = false
+      else
+        existing_capabilities.semanticTokensProvider.full = true
+      end
+    end,
+    filetypes = tsserver_filetypes,
+  }
 
-	vim.lsp.config("vtsls", {
-		filetypes = tsserver_filetypes,
-		settings = {
-			vtsls = {
-				tsserver = {
-					globalPlugins = {
-						vue_plugin,
-						{
-							name = "typescript-svelte-plugin",
-							location = vim.fn.stdpath("data") .. "/mason/packages/svelte-language-server",
-							languages = { "svelte" },
-							configNamespace = "typescript",
-							enableForWorkspaceTypeScriptVersions = true,
-						},
-					},
-					preferences = {
-						includeInlayFunctionLikeReturnTypeHints = false,
-					},
-				},
-			},
-			typescript = {
-				inlayHints = {
-					parameterNames = { enabled = "all" },
-					propertyDeclarationTypes = { enabled = true },
-					functionLikeReturnTypes = { enabled = true },
-					enumMemberValues = { enabled = true },
-					parameterTypes = { enabled = true },
-					variableTypes = { enabled = true },
-				},
-			},
-		},
-	})
+  vim.lsp.config("vtsls", {
+    filetypes = tsserver_filetypes,
+    settings = {
+      vtsls = {
+        tsserver = {
+          globalPlugins = {
+            vue_plugin,
+            {
+              name = "typescript-svelte-plugin",
+              location = vim.fn.stdpath("data") .. "/mason/packages/svelte-language-server",
+              languages = { "svelte" },
+              configNamespace = "typescript",
+              enableForWorkspaceTypeScriptVersions = true,
+            },
+          },
+          preferences = {
+            includeInlayFunctionLikeReturnTypeHints = false,
+          },
+        },
+      },
+      typescript = {
+        inlayHints = {
+          parameterNames = { enabled = "all" },
+          propertyDeclarationTypes = { enabled = true },
+          functionLikeReturnTypes = { enabled = true },
+          enumMemberValues = { enabled = true },
+          parameterTypes = { enabled = true },
+          variableTypes = { enabled = true },
+        },
+      },
+    },
+  })
 
-	vim.lsp.config("svelte", {
-		settings = {},
-	})
+  vim.lsp.config("svelte", {
+    settings = {},
+  })
 
-	vim.lsp.config("tsgo", {})
-	vim.lsp.config("ts_ls", ts_ls_config)
+  vim.lsp.config("tsgo", {})
+  vim.lsp.config("ts_ls", ts_ls_config)
 
-	vim.lsp.enable("vue_ls")
-	vim.lsp.enable("svelte")
-	--vim.lsp.enable("tsgo")
-	vim.lsp.enable("ts_ls")
-	--vim.lsp.enable("vtsls")
+  vim.lsp.enable("vue_ls")
+  vim.lsp.enable("svelte")
+  --vim.lsp.enable("tsgo")
+  vim.lsp.enable("ts_ls")
+  --vim.lsp.enable("vtsls")
 end
 
 return M
